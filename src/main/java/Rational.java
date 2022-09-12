1e27d61def4fdde5d80cab4a85491f124a812e16
Chaiyasit Apirakkitikul <chaiyasit.api@student.mahidol.ac.th>
2022-09-12 10:42:50 +0700

    Update Rational test and function

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -46,6 +46,9 @@ class Rational {
      */
     public void subtract(Rational x) {
         // to be completed
+        this.numerator = (this.numerator * x.denominator) - (x.numerator * this.denominator);
+        this.denominator = (this.denominator * x.denominator);
+        this.simplestForm();
     }
 
     /***
@@ -54,6 +57,9 @@ class Rational {
      */
     public void multiply(Rational x) { 
         // to be completed
+        this.numerator = (this.numerator * x.numerator);
+        this.denominator = (this.denominator * x.denominator);
+        this.simplestForm();
     }
 
     /***
@@ -62,6 +68,9 @@ class Rational {
      */
     public void divide(Rational x) {
         // to be completed
+        this.numerator = (this.numerator * x.denominator);
+        this.denominator = (this.denominator * x.numerator);
+        this.simplestForm();
     }
 
     /***
@@ -71,7 +80,13 @@ class Rational {
      */
     public boolean equals(Object x) {
         // to be completed
-        return true; // TODO: This needs to be modified.
+        Rational target = Rational.class.cast(x);
+        this.simplestForm();
+        target.simplestForm();
+        if(this.numerator == target.numerator && this.denominator == target.denominator){
+            return true;
+        }
+        return false; // TODO: This needs to be modified.
     }
 
     /***
@@ -82,7 +97,16 @@ class Rational {
      */
     public long compareTo(Object x) {
         // to be completed
-        return -1; // TODO: this needs to be modified.
+        Rational target = Rational.class.cast(x);
+        this.simplestForm();
+        target.simplestForm();
+        if (this.numerator == target.numerator && this.denominator == target.denominator) {
+            return 0;
+        } else if ((this.numerator * target.denominator) < (target.numerator * this.denominator)) {
+            return -1;
+        } else {
+            return 1; // TODO: this needs to be modified.
+        }
     }
 
     /***
@@ -91,7 +115,7 @@ class Rational {
      */
     public String toString() { 
         // to be completed
-        return ""; // TODO: This needs to be modified.
+        return this.numerator + "/" + this.denominator; // TODO: This needs to be modified.
     }
 
     public static void main(String[] args) {
--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -13,4 +13,101 @@ public class RationalTest {
         x.add(y);
         Assert.assertEquals(3, x.numerator);
     }
+    @Test
+    public void testSubtract(){
+        Rational x = new Rational(); // 2/4
+        Rational y = new Rational(); // 1/4
+        x.numerator = 2;
+        x.denominator = 4;
+        y.numerator = 1;
+        y.denominator = 4;
+        x.subtract(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(4, x.denominator);
+    }
+
+    @Test
+    public void testMultiply(){
+        Rational x = new Rational(); // 1/2
+        Rational y = new Rational(); // 1/3
+        x.numerator = 1;
+        x.denominator = 2;
+        y.numerator = 1;
+        y.denominator = 3;
+        x.multiply(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(6, x.denominator);
+    }
+
+    @Test
+    public void testDivide(){
+        Rational x = new Rational(); // 1/2
+        Rational y = new Rational(); // 1/3
+        x.numerator = 1;
+        x.denominator = 2;
+        y.numerator = 1;
+        y.denominator = 3;
+        x.divide(y);
+        Assert.assertEquals(3, x.numerator);
+        Assert.assertEquals(2, x.denominator);
+    }
+
+    @Test
+    public void testEqual(){  //Case Both Equal with same value
+        Rational x = new Rational(); // 1/2
+        Rational y = new Rational(); // 1/2
+        x.numerator = 1;
+        x.denominator = 2;
+        y.numerator = 2;
+        y.denominator = 4;
+        Assert.assertTrue(x.equals(y));
+    }
+    @Test
+    public void testEqual2(){ //Case Equal with difference form
+        Rational x = new Rational(); // 4/8
+        Rational y = new Rational(); // 12/24
+        x.numerator = 4;
+        x.denominator = 8;
+        y.numerator = 12;
+        y.denominator = 24;
+        Assert.assertTrue(x.equals(y));
+    }
+    @Test
+    public void testEqual3(){ //Case Equal with negative value
+        Rational x = new Rational(); // -1/2
+        Rational y = new Rational(); // -1/2
+        x.numerator = -1;
+        x.denominator = 2;
+        y.numerator = -1;
+        y.denominator = 2;
+        Assert.assertTrue(x.equals(y));
+    }
+    @Test
+    public void testEqual4(){ //Case Equal with negative value and difference form
+        Rational x = new Rational(); // -2/3
+        Rational y = new Rational(); // -4/6
+        x.numerator = -2;
+        x.denominator = 3;
+        y.numerator = -4;
+        y.denominator = 6;
+        Assert.assertTrue(x.equals(y));
+    }
+
+    @Test
+    public void testCompareTo(){
+        Rational x = new Rational(); // 4/8
+        Rational y = new Rational(); // 3/8
+        x.numerator = 4;
+        x.denominator = 8;
+        y.numerator = 3;
+        y.denominator = 8;
+        Assert.assertEquals(1, x.compareTo(y));
+    }
+    @Test
+    public void testToString(){
+        Rational x = new Rational(); // 4/8
+        x.numerator = 4;
+        x.denominator = 8;
+        Assert.assertEquals("4/8", x.toString());
+    }
 }
